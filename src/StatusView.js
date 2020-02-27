8a7500ee01ec97fbac36fcf5fc10e7dd2eea2acc
ASP19SCM59K <akumar72@hawk.iit.edu>
2020-02-27 11:33:38 -0500

    second commit

--- src/StatusView.js
+++ src/StatusView.js
@@ -21,6 +21,7 @@ class FormsAndInputs extends Component {
         return (
             <div>
                 <h1>Status Report</h1>
+                <h2>Abhishek Kumar</h2>
                 <form onSubmit={this.handleSubmit}>
                     <p><input type='text' placeholder='type here...' name='name' size='150' /></p>
                     <p><textarea id='txt' rows='30' cols='150' placeholder='start typing here...'></textarea></p>
