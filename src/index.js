9463e17ec63b1049aa909ff8ec8f43e1e3c7d066
ASP19SCM59K <akumar72@hawk.iit.edu>
2020-02-27 13:59:19 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,38 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import StatusView from './StatusView';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react componenet
 const App = function () {
     return (
         <div className="ui container comments">
             <StatusView></StatusView>
-            <CommentDetail author="sam" timeAgo="Today at 4:50PM" comment="Goooood!" />
-            <CommentDetail author="Alex" timeAgo="Today at 6:50PM" comment="Need to work more" />
-            <CommentDetail author="Jane" timeAgo="Today at 5:30PM" comment="I want to learn react" />
-            <CommentDetail author="Jhon" timeAgo="Today at 5:40PM" comment="Nice work!" />
+            <ApprovalCard>
+                <CommentDetail
+                    author="sam"
+                    timeAgo="Today at 4:50PM"
+                    comment="Goooood!" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 6:50PM"
+                    comment="Need to work more" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jane"
+                    timeAgo="Today at 5:30PM"
+                    comment="I want to learn react" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jhon"
+                    timeAgo="Today at 5:40PM"
+                    comment="Nice work!" />
+            </ApprovalCard>
         </div>
 
 
